382ffc9b1b34b84a2a709c49825e7dde77543c82
jalene05 <118020847+jalene05@users.noreply.github.com>
2022-12-14 10:57:22 -0500

    Update Final Paper Data Analysis.R

--- Final Paper Data Analysis.R
+++ Final Paper Data Analysis.R
@@ -13,15 +13,12 @@ options(scipen = 999)
 dat <- dat %>%
   select(participant, hitPercent, pointsGiven, horOrTilt, hitOrMiss, trialType, trialsNum, tasksNum, wallOrient, Sound)
 
-# Outlier analysis - Keep ppl who hit the ball more than 50% of the time. Exclude participants who hit the ball 50% or less of the time
+# Outlier analysis - Keep people who hit the ball more than 50% of the time. Exclude participants who hit the ball 50% or less of the time
 dat = dat %>% group_by(participant) %>%
   mutate(hitPercent2 = mean(hitOrMiss == "hit", na.rm = TRUE))  %>%
   filter(hitPercent2 > 0.5)
 
-
-
-
-# Linear Mixed Model (Repeated Measures ANOVA): x1 = Wall Orientation, x2 = Sound, y = Points Given 
+# Linear Mixed Model (Equivalent to Repeated Measures ANOVA): x1 = Wall Orientation, x2 = Sound, y = Points Given 
 # Recode Variables
 dat$Sound <- as.factor(dat$Sound)
 
@@ -31,12 +28,10 @@ dat$Sound <- dat$Sound %>%
 dat$wallOrient <- as.factor(dat$wallOrient)
 dat$pointsGiven <- as.numeric(dat$pointsGiven)
 
-
-# Linear Model/ANOVA
+# Linear Model
 Model3 = lmer(pointsGiven ~ wallOrient*Sound + (1 | participant), 
               data = dat)
-
-# Results of ANOVA
+# Results
 summary(Model3)
 
 
