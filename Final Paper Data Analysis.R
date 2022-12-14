245a34f318f7d30164f2ce50c725da66cf071d6f
jalene05 <118020847+jalene05@users.noreply.github.com>
2022-12-14 10:55:20 -0500

    Add files via upload

---
+++ Final Paper Data Analysis.R
@@ -0,0 +1,46 @@
+dat = read.csv("Final Mikula Results.csv", header=TRUE, na.strings=c("", "NA", " ", "N/A"))  
+
+require(tidyverse)
+require(lme4)
+require(lmerTest)
+require(ggplot2)
+require(rstatix)
+
+# Turn off scientific notation (optional)
+options(scipen = 999)
+
+# Keep Variables necessary for viewing and analyzing the data, remove unneeded variables
+dat <- dat %>%
+  select(participant, hitPercent, pointsGiven, horOrTilt, hitOrMiss, trialType, trialsNum, tasksNum, wallOrient, Sound)
+
+# Outlier analysis - Keep ppl who hit the ball more than 50% of the time. Exclude participants who hit the ball 50% or less of the time
+dat = dat %>% group_by(participant) %>%
+  mutate(hitPercent2 = mean(hitOrMiss == "hit", na.rm = TRUE))  %>%
+  filter(hitPercent2 > 0.5)
+
+
+
+
+# Linear Mixed Model (Repeated Measures ANOVA): x1 = Wall Orientation, x2 = Sound, y = Points Given 
+# Recode Variables
+dat$Sound <- as.factor(dat$Sound)
+
+dat$Sound <- dat$Sound %>%
+  recode("0.4" = "Sound", "5" = "No Sound")
+
+dat$wallOrient <- as.factor(dat$wallOrient)
+dat$pointsGiven <- as.numeric(dat$pointsGiven)
+
+
+# Linear Model/ANOVA
+Model3 = lmer(pointsGiven ~ wallOrient*Sound + (1 | participant), 
+              data = dat)
+
+# Results of ANOVA
+summary(Model3)
+
+
+#Descriptive Statistics Table of points earned by wall orientation and sound
+dat %>%
+  group_by(wallOrient, Sound) %>%
+  get_summary_stats(pointsGiven) 
